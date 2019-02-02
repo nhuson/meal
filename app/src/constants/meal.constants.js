00f870389157fede280484aa605a19c1e560af07
nhuson <nhuson1094@gmail.com>
2019-02-02 12:40:08 +0700

    add logout

---
+++ app/src/constants/meal.constants.js
@@ -0,0 +1,10 @@
+export const mealConstants = {
+    meal: {
+        MEAL_CREATE: 'MEAL_CREATE',
+        MEAL_UPDATE: 'MEAL_UPDATE',
+        MEAL_AVAILABLE: 'MEAL_AVAILABLE'
+    },
+    ingredient: {
+
+    }
+}
